d85c2454c6b1e738ca6286656bb25ec2b9b949c1
GipsyDangerM3 <mikael_dev@hotmail.com>
2022-03-09 07:41:01 -0300

    Adding more tests and allowing null parameter in the 'image' attribute in the product entity

--- CleanArchMvc.Domain.Tests/ProductUnitTest1.cs
+++ CleanArchMvc.Domain.Tests/ProductUnitTest1.cs
@@ -16,7 +16,7 @@ namespace CleanArchMvc.Domain.Tests
         }
 
         [Fact(DisplayName = "Create Product With Negative Id Value")]
-        public void CreateProduct_NegativeIdValue_DomainExceptionInvalidId()
+        public void CreateProduct_WithNegativeIdValue_DomainExceptionInvalidId()
         {
             Action action = () => new Product(-1, "Product Name", "Product Description", 9.99m, 99, "product image");
             action.Should()
@@ -24,7 +24,7 @@ namespace CleanArchMvc.Domain.Tests
         }
 
         [Fact(DisplayName = "Create Product With Short Name Value")]
-        public void CreateProduct_ShortNameValue_DomainExceptionShortName()
+        public void CreateProduct_WithShortNameValue_DomainExceptionShortName()
         {
             Action action = () => new Product(1, "Pr", "Product Description", 9.99m, 99, "product image");
             action.Should()
@@ -32,7 +32,7 @@ namespace CleanArchMvc.Domain.Tests
         }
 
         [Fact(DisplayName = "Create Product With Missing Name Value")]
-        public void CreateProduct_MissingNameValue_DomainExceptionRequiredName()
+        public void CreateProduct_WithMissingNameValue_DomainExceptionRequiredName()
         {
             Action action = () => new Product(1, "", "Product Description", 9.99m, 99, "product image");
             action.Should()
@@ -40,7 +40,7 @@ namespace CleanArchMvc.Domain.Tests
         }
 
         [Fact(DisplayName = "Create Product With Short Description Value")]
-        public void CreateProduct_ShortDescriptionValue_DomainExceptionShortDescription()
+        public void CreateProduct_WithShortDescriptionValue_DomainExceptionShortDescription()
         {
             Action action = () => new Product(1, "Product Name", "Pr", 9.99m, 99, "product image");
             action.Should()
@@ -48,7 +48,7 @@ namespace CleanArchMvc.Domain.Tests
         }
 
         [Fact(DisplayName = "Create Product With Missing Description Value")]
-        public void CreateProduct_MissingDescriptionValue_DomainExceptionRequiredDescription()
+        public void CreateProduct_WithMissingDescriptionValue_DomainExceptionRequiredDescription()
         {
             Action action = () => new Product(1, "Product Name", "", 9.99m, 99, "product image");
             action.Should()
@@ -56,7 +56,7 @@ namespace CleanArchMvc.Domain.Tests
         }
 
         [Fact(DisplayName = "Create Product With Invalid Price Value")]
-        public void CreateProduct_InvalidPriceValue_DomainExceptionInvalidPriceValue()
+        public void CreateProduct_WithInvalidPriceValue_DomainExceptionInvalidPriceValue()
         {
             Action action = () => new Product(1, "Product Name", "Product Description", -9.99m, 99, "product image");
             action.Should()
@@ -64,7 +64,7 @@ namespace CleanArchMvc.Domain.Tests
         }
 
         [Fact(DisplayName = "Create Product With Invalid Stock Value")]
-        public void CreateProduct_InvalidStockValue_DomainExceptionInvalidStockValue()
+        public void CreateProduct_WithInvalidStockValue_DomainExceptionInvalidStockValue()
         {
             Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, -99, "product image");
             action.Should()
@@ -72,22 +72,36 @@ namespace CleanArchMvc.Domain.Tests
         }
 
         [Fact(DisplayName = "Create Product With Long Image Name")]
-        public void CreateProduct_LongImageName_DomainExceptionLongImageName()
+        public void CreateProduct_WithLongImageName_DomainExceptionLongImageName()
         {
             Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, "5R0BSYFSVSwOEfvPGXzQ4Fk49W6y7MoTMOdD8PTgkErBzsb7Hp82JD6IPvbEpqTBL2En7iYwZ8daCBgVqHJ0r1EwK24RKIiwK6HcAC1ShP1bz44cduoAWvQKBDZfg4CAJU9TB5OTi5SINqMbxIiwyUff6EWFDx4EhrjldfEeO2nB51ULVFAZDkmEERNy1ovoQ5g1L6iYE6HyTgdhoTv1v4szqiMOVCui13nvXo00lk083u07gpvDm5PExDC");
             action.Should()
                 .Throw<CleanArchMvc.Domain.Validations.DomainExceptionValidation>().WithMessage("Invalid Image name, too long, maximum 250 characters");
         }
 
-        [Fact(DisplayName = "Create Product With Empty Image Name")]
-        public void CreateProduct_EmptyImageName_DomainExceptionLongImageName()
+        [Fact(DisplayName = "Create Product With Null Image Name")]
+        public void CreateProduct_WithNullImageName_NoDomainException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, null);
+            action.Should()
+                .NotThrow<CleanArchMvc.Domain.Validations.DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "Create Product With Null Image Name")]
+        public void CreateProduct_WithNullImageName_NoNullReferenceException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, null);
+            action.Should()
+                .NotThrow<NullReferenceException>();
+        }
+
+        [Fact(DisplayName = "Create Product With Null Image Name")]
+        public void CreateProduct_WithEmptyImageName_NoDomainException()
         {
             Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, "");
             action.Should()
                 .NotThrow<CleanArchMvc.Domain.Validations.DomainExceptionValidation>();
         }
-
-
 
     }
 }
