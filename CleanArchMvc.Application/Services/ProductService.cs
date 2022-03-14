c431703188211163d03a9872149c8c91470fc9d7
GipsyDangerM3 <mikael_dev@hotmail.com>
2022-03-14 16:45:24 -0300

    Removing duplicate code

--- CleanArchMvc.Application/Services/ProductService.cs
+++ CleanArchMvc.Application/Services/ProductService.cs
@@ -42,18 +42,6 @@ namespace CleanArchMvc.Application.Services
             return _mapper.Map<ProductDTO>(result);
         }
 
-        public async Task<ProductDTO> GetProductCategoryAsync(int? id)
-        {
-            var productByIdQuery = new GetProductByIdQuery(id.Value);
-            if (productByIdQuery == null)
-            {
-                throw new Exception($"Entity could not be loaded.");
-            }
-
-            var result = await _mediator.Send(productByIdQuery);
-            return _mapper.Map<ProductDTO>(result);
-        }
-
         public async Task<IEnumerable<ProductDTO>> GetProductsAsync()
         {
             var productsQuery = new GetProductsQuery();
