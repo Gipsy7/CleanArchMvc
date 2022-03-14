e1af00165861fa5382820ab674539f5caa893fa7
GipsyDangerM3 <mikael_dev@hotmail.com>
2022-03-14 20:51:12 -0300

    Fix in ProductDTO

--- CleanArchMvc.Application/DTOs/ProductDTO.cs
+++ CleanArchMvc.Application/DTOs/ProductDTO.cs
@@ -13,29 +13,38 @@ namespace CleanArchMvc.Application.DTOs
     public class ProductDTO
     {
         public int Id { get; set; }
-        [Required(ErrorMessage = "The name is required")]
+
+        [Required(ErrorMessage = "The Name is Required")]
         [MinLength(3)]
         [MaxLength(100)]
-        public string Name { get; private set; }
-        [Required(ErrorMessage = "The description is required")]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The Description is Required")]
         [MinLength(5)]
         [MaxLength(200)]
-        public string Description { get; private set; }
-        [Required(ErrorMessage = "The price is required")]
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "The Price is Required")]
         [Column(TypeName = "decimal(18,2)")]
         [DisplayFormat(DataFormatString = "{0:C2}")]
         [DataType(DataType.Currency)]
         [DisplayName("Price")]
-        public decimal Price { get; private set; }
-        [Required(ErrorMessage = "The stock is required")]
-        [Range(1,9999)]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "The Stock is Required")]
+        [Range(1, 9999)]
         [DisplayName("Stock")]
-        public int Stock { get; private set; }
+        public int Stock { get; set; }
+
         [MaxLength(250)]
         [DisplayName("Product Image")]
-        public string Image { get; private set; }
+        public string Image { get; set; }
+
+        public Category Category { get; set; }
+
         [DisplayName("Categories")]
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
     }
 }
