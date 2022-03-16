9892747097d69f9e2e021f2b5fe796478164b194
GipsyDangerM3 <mikael_dev@hotmail.com>
2022-03-15 21:34:45 -0300

    Configuring Swagger to use the JWT token

--- CleanArchMvc.API/Startup.cs
+++ CleanArchMvc.API/Startup.cs
@@ -28,15 +28,11 @@ namespace CleanArchMvc.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddInfrastructureAPI(Configuration);
-
             //Enable authentication and validate token
             services.AddInfrastructureJWT(Configuration);
-
+            services.AddInfrastructureSwagger();
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CleanArchMvc.API", Version = "v1" });
-            });
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
